2c2769667cf2bd9b9fd586d5ff331e1fc8070dbe
Mikhail Sulim <mika-1984@yandex.ru>
2023-06-10 04:16:06 +0300

    feature: сверстать блок portfolio

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
               <AboutProject/>
               <Techs/>
               <AboutMe/>
+              <Portfolio/>
             </>
           }
         />
