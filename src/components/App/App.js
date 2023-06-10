17340f9ada172fc00b5de6339630b9e20fa8f13a
Mikhail Sulim <mika-1984@yandex.ru>
2023-06-10 03:42:42 +0300

    feature: сверстать блок aboutme

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
               <Promo />
               <AboutProject/>
               <Techs/>
+              <AboutMe/>
             </>
           }
         />
